d9757eaabefcf1bfe8eb7b667422372e1504031f
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-10-23 21:32:27 +0900

    chat.jsの作成

--- app/javascript/chat.js
+++ app/javascript/chat.js
@@ -7,15 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
           const talkedText = event.results[0][0].transcript;
           document.getElementById('talked-content').innerText = talkedText;
 
-          // 必要であればAPIにテキストを送信する処理を追加
-          // fetch('/your_api_endpoint', {
-          //   method: 'POST',
-          //   headers: {
-          //     'Content-Type': 'application/json',
-          //     'Authorization': 'Bearer api-key'
-          //   },
-          //   body: JSON.stringify({ text: talkedText })
-          // });
       };
 
       recognition.start();

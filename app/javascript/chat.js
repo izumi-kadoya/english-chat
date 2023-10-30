c5bf6c7e28bd5bfee8ec73e0d51d6bb397ba6aa4
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-10-30 14:12:03 +0900

    一時停止ボタン

--- app/javascript/chat.js
+++ app/javascript/chat.js
@@ -2,13 +2,45 @@ document.addEventListener('DOMContentLoaded', function() {
   const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
   recognition.lang = 'en-US';
 
+  recognition.onresult = function(event) {
+      const talkedText = event.results[0][0].transcript;
+      document.getElementById('talked-content').innerText = talkedText;
+  };
+
   document.getElementById('start-recording').addEventListener('click', function() {
-      recognition.onresult = function(event) {
-          const talkedText = event.results[0][0].transcript;
-          document.getElementById('talked-content').innerText = talkedText;
+      navigator.mediaDevices.getUserMedia({ audio: true })
+          .then(function(stream) {
+              // Audio processing setup
+              let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+              let analyser = audioContext.createAnalyser();
+              let microphone = audioContext.createMediaStreamSource(stream);
+              let javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+              
+              analyser.smoothingTimeConstant = 0.8;
+              analyser.fftSize = 1024;
+              
+              microphone.connect(analyser);
+              analyser.connect(javascriptNode);
+              javascriptNode.connect(audioContext.destination);
 
-      };
+              javascriptNode.onaudioprocess = function() {
+                  let array = new Uint8Array(analyser.frequencyBinCount);
+                  analyser.getByteFrequencyData(array);
+                  let values = 0;
+                  let length = array.length;
+                  for (let i = 0; i < length; i++) {
+                      values += (array[i]);
+                  }
 
-      recognition.start();
+                  let average = values / length;
+                  document.querySelector('.volume-level').style.width = average + "%";
+              }
+
+              // Start voice recognition
+              recognition.start();
+          })
+          .catch(function(err) {
+              console.log('The following error occurred: ' + err);
+          });
   });
 });
